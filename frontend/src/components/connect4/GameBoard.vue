17b40d57d70d5f1e5b8ab42e67f7eb9f83b4193f
Florian Schmid <melonencool@gmail.com>
2024-11-16 23:31:13 +0100

    add hover preview

--- frontend/src/components/connect4/GameBoard.vue
+++ frontend/src/components/connect4/GameBoard.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import GameChip from './GameChip.vue';
 
 defineProps({ board: Array<Array<number>>, onClick: Function });
+  const hoverArray = ref([0, 0, 0, 0, 0, 0, 0]);
 
 function getColor(value: number): string {
   switch (value) {
@@ -16,11 +18,17 @@ function getColor(value: number): string {
 </script>
 
 <template>
+  <div class="flex flex-col">
+  <div class="flex flex-row p-4"> 
+    <GameChip v-for="n in 7" :key="n" :class="'w-16 aspect-square mx-2 ' + (hoverArray[n - 1] == 0 ? 'invisible' : 'visible')" />
+  </div>
   <div v-if="board" class="flex flex-row bg-gray-500 w-fit rounded-2xl p-4">
     <div  v-for="c, c_index in (board[0] || []).length" 
           :key="c_index" 
           @click="onClick!(c_index)"
           class="hover:cursor-pointer"
+          @mouseover="hoverArray[c_index] = 1"
+          @mouseleave="hoverArray[c_index] = 0"
       >
       <div
         v-for="r, r_index in board.length"
@@ -30,5 +38,6 @@ function getColor(value: number): string {
         <GameChip v-if="board[r_index][c_index] != 0" />
       </div>
     </div>
+  </div>
   </div>
 </template>
