848d1c52adddca71a78efc61c6e882ee28556682
Florian Schmid <melonencool@gmail.com>
2024-11-17 15:47:00 +0100

    add drop sound

--- frontend/src/views/connect4/GameView.vue
+++ frontend/src/views/connect4/GameView.vue
@@ -11,6 +11,10 @@ import { onMounted, onUnmounted, ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import Chatbox from '@/components/Chatbox.vue'
 import ChatMessage from '@/components/ChatMessage.vue'
+import dropSfx from "@/assets/audio/chipdrop.mp3";
+import { useSettingsStore } from '@/stores/settingStore'
+
+const chipDrop = new Audio(dropSfx);
 
 const route = useRoute();
 const currentRouter = useRouter();
@@ -22,6 +26,7 @@ const playerSymbol = ref('');
 const gameWinner = ref(0);
 const chat = ref(["Welcome to Connect Four!"]);
 const board = ref({state: Array.from({ length: 6 }, () => Array.from({ length: 7 }, () => 0))});
+const isSoundEnabled = ref(false);
 
 onMounted(async () => {
     // validate game id
@@ -73,6 +78,8 @@ function gameStateUpdate(state: Array<Array<number>>) {
       for (let j = 0; j < state[0].length; j++) {
         if (state[i][j] != board.value.state[i][j]) {
           gameWinner.value = checkForWin(i, j, state); 
+          if (isSoundEnabled.value)
+            chipDrop.play();
           break;
         }
       }
@@ -182,6 +189,8 @@ function gameStateUpdate(state: Array<Array<number>>) {
 </script>
 
 <template>
+  <div class="flex flex-col justify-center">
+    <!-- Poker Chip Drop by Za-Games -- https://freesound.org/s/540369/ -- License: Creative Commons 0 -->
     <InfoDisplay
         :is-running="isRunning"
         :is-spectator="isSpectator"
@@ -202,7 +211,14 @@ function gameStateUpdate(state: Array<Array<number>>) {
       </SimpleButton>
     </div>
 
+    <div v-if="isRunning" class="flex flex-row justify-center items-center">
+      <span class="text-xl mr-2">Sound: </span>
+      <svg v-if="isSoundEnabled" @click="isSoundEnabled = !isSoundEnabled" class="w-8 hover:cursor-pointer" viewBox="-0.5 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12.5493 4.50005C11.3193 4.04005 8.70926 5.49996 6.54926 7.40996H4.94922C3.88835 7.40996 2.87093 7.83145 2.12079 8.58159C1.37064 9.33174 0.949219 10.3491 0.949219 11.41V13.41C0.949219 14.4708 1.37064 15.4883 2.12079 16.2385C2.87093 16.9886 3.88835 17.41 4.94922 17.41H6.54926C8.65926 19.35 11.2693 20.78 12.5493 20.33C14.6493 19.55 14.9992 15.33 14.9992 12.41C14.9992 9.48996 14.6493 5.28005 12.5493 4.50005Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M20.6602 6.71997C22.1593 8.22011 23.0015 10.2542 23.0015 12.375C23.0015 14.4958 22.1593 16.5299 20.6602 18.03" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M18.5391 15.95C19.4764 15.0123 20.003 13.7407 20.003 12.4149C20.003 11.0891 19.4764 9.81764 18.5391 8.88" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+      <svg v-if="!isSoundEnabled" @click="isSoundEnabled = !isSoundEnabled" class="w-8 hover:cursor-pointer" viewBox="-0.5 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M10.9395 17.72C12.9395 19.5 15.3895 20.72 16.5495 20.33C18.6495 19.55 18.9995 15.3299 18.9995 12.4099C18.9995 11.5999 18.9995 10.68 18.8895 9.77002" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M18.1292 6.28008C18.0012 5.89129 17.795 5.53273 17.5233 5.22661C17.2516 4.9205 16.9201 4.67327 16.5493 4.50005C15.3193 4.04005 12.7093 5.49996 10.5493 7.40996H8.94922C7.88835 7.40996 6.87093 7.83145 6.12079 8.58159C5.37064 9.33174 4.94922 10.3491 4.94922 11.41V13.41C4.9489 14.1811 5.17151 14.936 5.59021 15.5835C6.00892 16.2311 6.60585 16.7438 7.3092 17.06" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M22 2.42004L2 22.42" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+    </div>
+
     <Chatbox v-if="isRunning">
       <ChatMessage v-for='(message, m_index) in chat' :message='message' :key="m_index"/>
     </Chatbox>
+  </div>
 </template>
