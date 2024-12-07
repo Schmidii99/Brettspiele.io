f3d89ba6070e2bb11b69f60ee7cc35447ce33c80
Florian Schmid <melonencool@gmail.com>
2024-12-07 03:15:56 +0100

    update datenschutz link

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -39,5 +39,5 @@
         Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p><br> 
         Website Impressum erstellt durch <a href="https://www.impressum-generator.de">impressum-generator.de</a> von der <a href="https://www.kanzlei-hasselbach.de/" rel="nofollow">Kanzlei Hasselbach</a>
         </div>
-        <a class="text-2xl font-bold my-4 hover:text-blue-500 underline" target="_blank" href="/public/datenschutz.pdf">Datenschutzerklärung</a>
+        <a class="text-2xl font-bold my-4 hover:text-blue-500 underline" target="_blank" href="/datenschutz.pdf">Datenschutzerklärung</a>
 </template>
