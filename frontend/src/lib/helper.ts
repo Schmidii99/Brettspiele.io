aa73941ca27c19efd28faf991af0192b1cd16d39
Florian Schmid <melonencool@gmail.com>
2025-01-14 14:07:50 +0100

    add getRandomInt function

--- frontend/src/lib/helper.ts
+++ frontend/src/lib/helper.ts
@@ -24,3 +24,16 @@ export function copyToClipboard(text: string, showAlert: boolean = true) {
 }
 
 export function getFullLink(): string { return window.location.href; }
+
+ /**
+ * Returns a random integer between min (inclusive) and max (inclusive).
+ * The value is no lower than min (or the next integer greater than min
+ * if min isn't an integer) and no greater than max (or the next integer
+ * lower than max if max isn't an integer).
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+export function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
